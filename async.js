8ee97d5d5973956c5229317e23dcfba4c6f39cf8
Divyaudari <divyaudari7@gmail.com>
2025-01-21 13:52:52 +0530

    adding files to git

---
+++ async.js
@@ -0,0 +1,11 @@
+// console.log(10);
+// console.log(20);
+// setTimeout(()=>{console.lOg(30)},6000);
+//  setInterval(()=>{
+//     document.writeln("mru");
+//      console.l0g(30);
+//  },1500)
+// console.log(40);
+// console.log(50);
+
+//!promise
