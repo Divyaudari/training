65c17f922d193e940b3f186398ef09f6aaca2acb
Divyaudari <divyaudari7@gmail.com>
2025-01-22 13:02:56 +0530

    pushing files to git

--- async.js
+++ async.js
@@ -9,3 +9,32 @@
 // console.log(50);
 
 //!promise
+
+
+//! API fetching
+
+function fetchUsers(){
+    let response =fetch("https://jsonplaceholder.typicode.com/users");
+    response.then(result=>{
+        // console.log(result.json());
+        return result.json().then(data=>{
+            console.log(data);
+            let store = document.getElementById("store");
+            console.log(store);
+            data.map(user=>
+            {
+                store.innerHTML += `
+                <tr>
+                <td>${user.id}</td>
+                <td>${user.name}</td>
+                <td>${user.email}</td>
+                <td>${user.company.name}</td>
+                </tr>
+                `
+            }
+            )
+        })
+    })
+    .catch(err=>console.log(err))
+}
+fetchUsers();
